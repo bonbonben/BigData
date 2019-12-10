2bde4447a347b57d5f93296e81630d9673b4a401
Ben Hung <hben8352@gmail.com>
2019-12-10 17:01:15 -0500

    Update task2.py

--- Project/task2.py
+++ Project/task2.py
@@ -65,7 +65,7 @@ if __name__ == "__main__":
     output = []
     for file in file_list:
         temp_output = dict()
-        if file == "6ypq-ih9a.BOROUGH.txt.gz":
+        if file == "5uac-w243.PREM_TYP_DESC.txt.gz":
             inFile = path + file
             print("File Name: ", file)
             fileName = file.split('.')[1]
@@ -84,7 +84,56 @@ if __name__ == "__main__":
             borough_list = borough_list + lst
             boro = spark.createDataFrame(borough_list, StringType())
             boro.createOrReplaceTempView("boro")
-
+			
+            school_level_list = ["K-2", "K-3", "K-8", "ELEMENTARY", "MIDDLE", "HIGH SCHOOL TRANSFER", "HIGH SCHOOL", "D75","YABC"]
+            lst2 = [x.lower() for x in school_level_list]
+            school_level_list = school_level_list + lst2
+            level = spark.createDataFrame(school_level_list, StringType())
+            level.createOrReplaceTempView("level")
+			
+            vehicle_type_list = ["FIRE", "CONV", "SEDN", "SUBN", "4DSD", "2DSD", "H/WH", "ATV", "MCY", "H/IN", "LOCO", "RPLC",\
+			"AMBU", "P/SH", "RBM", "R/RD", "RD/S", "S/SP", "SN/P", "TRAV", "MOBL", "TR/E", "T/CR", "TR/C", "SWT",\
+			"W/DR", "W/SR", "FPM", "MCC", "EMVR", "TRAC", "DELV", "DUMP", "FLAT", "PICK", "STAK", "TANK",\
+			"REFG", "TOW", "VAN", "UTIL", "POLE", "BOAT", "H/TR", "SEMI", "TRLR", "LTRL", "LSVT", "BUS", "LIM",\
+			"HRSE", "TAXI", "DCOM", "CMIX", "MOPD", "MFH", "SNOW", "LSV"]
+            lst3 = [x.lower() for x in vehicle_type_list]
+            vehicle_type_list = vehicle_type_list + lst3
+            vehicle = spark.createDataFrame(vehicle_type_list, StringType())
+            vehicle.createOrReplaceTempView("vehicle")
+            predict = udf(lambda x: predict_category(x))
+			
+            car_make_list = ["ACUR", "ALFA", "AMGN", "AMER", "ASTO", "AUDI", "AUST", "AVTI", "AUTU", "BENT", "BERO", "BLUI",\
+			"BMW", "BRIC", "BROC", "BSA", "BUIC", "CADI", "CHEC", "CHEV", "CHRY", "CITR", "DAEW", "DAIH", "DATS",\
+			"DELO", "DESO", "DIAR", "DINA", "DIVC", "DODG", "DUCA", "EGIL", "EXCL", "FERR", "FIAT", "FORD", "FRHT",\
+			"FWD", "GZL", "GMC", "GRUM", "HD", "HILL", "HINO", "HOND", "HUDS", "HYUN", "CHRY", "INFI", "INTL",\
+			"ISU", "IVEC", "JAGU", "JENS", "AMER", "AMER", "KAWK", "KW", "KIA", "LADA", "LAMO", "LNCI", "LNDR",\
+			"LEXS", "LINC", "LOTU", "MACK", "MASE", "MAYB", "MAZD", "MCIN", "MERZ", "MERC", "MERK", "MG", "MITS",\
+			"MORG", "MORR", "MOGU", "NAVI", "NEOP", "NISS", "NORT", "OLDS", "OPEL", "ONTR", "OSHK", "PACK", "PANZ",\
+			"PTRB", "PEUG", "PLYM","PONT", "PORS", "RELA", "RENA", "ROL", "SAA", "STRN", "SCAN", "SIM", "SIN",\
+			"STLG", "STU", "STUZ", "SUBA", "SUNB", "SUZI", "THMS", "TOYT", "TRIU", "TVR", "UD", "VCTY", "VOLK",\
+			"VOLV", "WSTR", "WHIT", "WHGM", "AMER", "YAMA", "YUGO"]
+            lst4 = [x.lower() for x in car_make_list]
+            car_make_list = car_make_list + lst4
+            make = spark.createDataFrame(car_make_list, StringType())
+            make.createOrReplaceTempView("make")
+            predict = udf(lambda x: predict_category(x))
+			
+            area_of_study_list = ["ARCHITECTURE", "SCIENCE", "ART", "TEACHING", "TEACHING", "BUSINESS", "COMMUNICATIONS", "COSMETOLOGY",\
+			"ENGINEERING", "HUMANITIES", "TECHNOLOGY", "HEALTH", "ECONOMICS", "ENVIRONMENT", "ALGEBRA", "CHEMISTRY",\
+			"ENGLISH", "MATH","SOCIAL STUDIES"]
+            lst5 = [x.lower() for x in area_of_study_list]
+            area_of_study_list = area_of_study_list + lst5
+            area = spark.createDataFrame(area_of_study_list, StringType())
+            area.createOrReplaceTempView("area")
+            predict = udf(lambda x: predict_category(x))
+			
+            location_type_list = ["BUILDING", "AIRPORT", "ATM", "BANK", "CLUB", "BRIDGE", "TERMINAL", "STORE", "OFFICE",\
+			"STATION", "HOSPITAL", "RESIDENCE", "RESTAURANT", "TUNNEL", "PARK", "SHELTER", "MAILBOX", "SCHOOL", "STREET",\
+			"TRANSIT", "STOP", "FACTORY"]
+            lst6 = [x.lower() for x in location_type_list]
+            location_type_list = location_type_list + lst6
+            location = spark.createDataFrame(location_type_list, StringType())
+            location.createOrReplaceTempView("location")
             predict = udf(lambda x: predict_category(x))
 
             #phone
@@ -134,6 +183,38 @@ if __name__ == "__main__":
                 if borough.count() != col_count:
                     borough.createOrReplaceTempView("borough")
                     check = spark.sql("select _c0 from col except (select _c0 from borough)")
+                    check = check.withColumn('prediction', predict('_c0'))
+			#school_level
+            elif re.match(r"^\w*level\w*", fileName):
+                school_level = spark.sql("select c._c0 from col c, level l where c._c0 = l.value")
+                school_level.show()
+                if school_level.count() != col_count:
+                    school_level.createOrReplaceTempView("school_level")
+                    check = spark.sql("select _c0 from col except (select _c0 from school_level)")
+                    check = check.withColumn('prediction', predict('_c0'))
+            #vehicle_type
+            elif re.match(r"^\w*vehicle\w*", fileName):
+                vehicle_type = spark.sql("select c._c0 from col c, vehicle l where c._c0 = l.value")
+                vehicle_type.show()
+                if vehicle_type.count() != col_count:
+                    vehicle_type.createOrReplaceTempView("vehicle_type")
+                    check = spark.sql("select _c0 from col except (select _c0 from vehicle_type)")
+                    check = check.withColumn('prediction', predict('_c0'))
+            #car_make
+            elif re.match(r"^\w*make\w*", fileName):
+                car_make = spark.sql("select c._c0 from col c, make l where c._c0 = l.value")
+                car_make.show()
+                if car_make.count() != col_count:
+                    car_make.createOrReplaceTempView("car_make")
+                    check = spark.sql("select _c0 from col except (select _c0 from car_make)")
+                    check = check.withColumn('prediction', predict('_c0'))
+            #area_of_study
+            elif re.match(r"^\w*interest\w*", fileName):
+                area_of_study = spark.sql("select c._c0 from col c, area l where c._c0 = l.value")
+                area_of_study.show()
+                if area_of_study.count() != col_count:
+                    area_of_study.createOrReplaceTempView("area_of_study")
+                    check = spark.sql("select _c0 from col except (select _c0 from area_of_study)")
                     check = check.withColumn('prediction', predict('_c0'))
             else:
                 check = col.withColumn('prediction', predict('_c0'))
